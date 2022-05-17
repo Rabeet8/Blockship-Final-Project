9e865ddd94964110e0b41cf4abfa7f69b60f7794
Syed Rabeet <92630941+Rabeet8@users.noreply.github.com>
2022-05-17 13:38:24 +0500

    Update index.js

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,7 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x4F5Ba9350f5238BB98A2ff0B287f6ae97dFEA8Cb";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-rabeet.vercel.app/api/";
+const METADATA_URL = "https://blockship-final-project.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
