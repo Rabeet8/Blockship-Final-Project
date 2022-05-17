df1e8de860143c1ad781a3a11ab53f8b59961b21
Syed Rabeet <92630941+Rabeet8@users.noreply.github.com>
2022-05-17 22:04:18 +0500

    Update index.js

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x4F5Ba9350f5238BB98A2ff0B287f6ae97dFEA8Cb";
-// URL to extract Metadata for a Crypto Dev NFT
+// URL to extract Metadata for a  NFT
 const METADATA_URL = "https://blockship-final-project.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
